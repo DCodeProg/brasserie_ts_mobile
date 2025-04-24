1c65915de06b91abc7a3d27d0526296fa2fbf3a2
DCodeProg <danael.legrand@outlook.fr>
2025-04-24 11:01:41 +0200

    feat: init supabase client

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'core/secrets/app_secrets.dart';
 import 'core/shared/cubit/theme_cubit.dart';
 
 GetIt getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
+  await dotenv.load(fileName: '.env');
+
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  getIt.registerLazySingleton(() => supabase.client);
+
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory:
         kIsWeb
