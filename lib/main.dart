bd9604e266a6fdff251609683a513fda0a7ce146
DCodeProg <danael.legrand@outlook.fr>
2025-04-24 10:58:40 +0200

    feat: implement dependencies injection with get_it for theme cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
 
-import 'package:brasserie_ts_mobile/core/theme/app_theme.dart';
-import 'package:brasserie_ts_mobile/core/theme/text_theme.dart';
-import 'package:brasserie_ts_mobile/core/shared/cubit/theme_cubit.dart';
+import 'init_dependencies.dart';
+import 'core/theme/app_theme.dart';
+import 'core/theme/text_theme.dart';
+import 'core/shared/cubit/theme_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory:
-        kIsWeb
-            ? HydratedStorageDirectory.web
-            : HydratedStorageDirectory((await getTemporaryDirectory()).path),
-  );
+  await initDependencies();
 
-  runApp(const MainApp());
+  runApp(
+    BlocProvider(create: (context) => getIt<ThemeCubit>(), child: MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -29,18 +24,15 @@ class MainApp extends StatelessWidget {
     TextTheme textTheme = createTextTheme(context, "Noto Sans", "Rye");
     MaterialTheme theme = MaterialTheme(textTheme);
 
-    return BlocProvider(
-      create: (context) => ThemeCubit(),
-      child: BlocBuilder<ThemeCubit, ThemeMode>(
-        builder: (context, themeMode) {
-          return MaterialApp(
-            theme: theme.light(),
-            darkTheme: theme.dark(),
-            themeMode: themeMode,
-            home: Scaffold(),
-          );
-        },
-      ),
+    return BlocBuilder<ThemeCubit, ThemeMode>(
+      builder: (context, themeMode) {
+        return MaterialApp(
+          theme: theme.light(),
+          darkTheme: theme.dark(),
+          themeMode: themeMode,
+          home: Scaffold(),
+        );
+      },
     );
   }
 }
