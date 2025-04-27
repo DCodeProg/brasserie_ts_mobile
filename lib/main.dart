ea2631fb9ba1595060b5e7eaaebac025b282cac7
DCodeProg <danael.legrand@outlook.fr>
2025-04-28 00:53:37 +0200

    feat: handle navigation with go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'init_dependencies.dart';
+import 'core/router/app_router.dart';
+import 'core/shared/cubit/theme_cubit.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/text_theme.dart';
-import 'core/shared/cubit/theme_cubit.dart';
+import 'init_dependencies.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,11 +31,12 @@ class MainApp extends StatelessWidget {
 
     return BlocBuilder<ThemeCubit, ThemeMode>(
       builder: (context, themeMode) {
-        return MaterialApp(
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
           theme: theme.light(),
           darkTheme: theme.dark(),
           themeMode: themeMode,
-          home: Scaffold(),
+          routerConfig: appRouter,
         );
       },
     );
